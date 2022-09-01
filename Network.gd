73989b85b8fd21a1851e40d17c0cd964acec1f27
wile1005 <79308836+wile1005@users.noreply.github.com>
2022-09-01 13:13:51 +0200

    Added game :)
    now is game

---
+++ Network.gd
@@ -0,0 +1,17 @@
+extends Node
+
+const DEFAULT_PORT = 2896
+const MAX_CLIENTS = 6
+
+var server = null
+var client = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
