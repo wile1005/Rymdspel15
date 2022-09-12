c9f261d90cf060d1b402d9dd18e09df757cc9526
wile1005 <79308836+wile1005@users.noreply.github.com>
2022-09-12 14:31:39 +0200

    Added epic multiplayer menu
    man i love multiplayer
    (also added le epic gamer font)

--- Scripts/Multiplayer-menu.gd
+++ Scripts/Multiplayer-menu.gd
@@ -1,27 +1,45 @@
 extends VBoxContainer
 
+onready var multiplayer_config_ui = $Multiplayer_configure
+onready var server_ip_adress = $Multiplayer_configure/server_ip_adress
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var device_ip_adress = $Multiplayer_configure/Devic_ip_adress
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	visible = false
-	pass # Replace with function body.
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connect_to_server")
+	
+	device_ip_adress.text = Network.ip_adress
+
+func _player_conncted(id) -> void:
+	print("Player " + str(id) + " has connected")
+
+func _player_disconnected(id) -> void:
+	print("Player " + str(id) + " has disconnected")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_Create_server_pressed():
+	multiplayer_config_ui.hide()
+	Network.create_server()
+
+
+func _on_Join_server_pressed():
+	if server_ip_adress.text != "":
+		multiplayer_config_ui.hide()
+		Network.ip_adress = server_ip_adress.text
+		Network.join_server()
+	pass
 
 
 func _on_MultiplayerButton_pressed():
 	visible = true
-	pass # Replace with function body.
+	pass
 
 
 func _on_BackButton_pressed():
 	visible = false
-	pass # Replace with function body.
+	pass 
+
--- Scripts/Network.gd
+++ Scripts/Network.gd
@@ -6,12 +6,35 @@ const MAX_CLIENTS = 6
 var server = null
 var client = null
 
+var ip_adress = ""
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if OS.get_name() == "Windows":
+		ip_adress = IP.get_local_addresses()[3]
+	elif OS.get_name() == "Android":
+		ip_adress = IP.get_local_addresses()[0]
+	else:
+		ip_adress = IP.get_local_addresses()[3]
+	
+	for ip in IP.get_local_addresses():
+		if ip.begins_with("192.126."):
+			ip_adress = ip
+	
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
 
+func create_server() -> void:
+	server = NetworkedMultiplayerENet.new()
+	server.create_server(DEFAULT_PORT, MAX_CLIENTS)
+	get_tree().set_network_peer(server)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func join_server() -> void:
+	server = NetworkedMultiplayerENet.new()
+	server.create_client(ip_adress, DEFAULT_PORT)
+	get_tree().set_network_peer(client)
+
+func _connected_to_server() -> void:
+	print("Succsesfully connected to server")
+
+func _server_disconnected() -> void:
+	print("Disconnected from the server")
