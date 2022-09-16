352db596b94b0ec7390264ee731544093057188e
wile1005 <79308836+wile1005@users.noreply.github.com>
2022-09-16 12:04:03 +0200

    a

--- Scripts/Main-menu.gd
+++ Scripts/Main-menu.gd
@@ -1,20 +1,9 @@
 extends VBoxContainer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = true
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_SettingsButton_pressed():
@@ -29,4 +18,8 @@ func _on_BackButton_pressed():
 
 func _on_MultiplayerButton_pressed():
 	visible = false
+	pass # Replace with function body.
+
+
+func _on_OptionButton_item_selected(index):
 	pass # Replace with function body.
